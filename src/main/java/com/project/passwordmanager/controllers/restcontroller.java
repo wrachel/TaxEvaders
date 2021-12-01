7c2ead9c1145efd9d1af84b124e0659d4629f87f
wiz124 <hli16492@gmail.com>
2021-12-01 09:02:46 -0800

    snfdk;w';

---
+++ src/main/java/com/project/passwordmanager/controllers/restcontroller.java
@@ -0,0 +1,7 @@
+package com.project.passwordmanager.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class restcontroller {
+
+}
