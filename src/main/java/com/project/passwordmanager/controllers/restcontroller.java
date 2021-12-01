4bf6d69d52e1a8c9f81caa5b4d6328394260a75c
wiz124 <hli16492@gmail.com>
2021-12-01 09:07:20 -0800

    i think i did rest controller

--- src/main/java/com/project/passwordmanager/controllers/restcontroller.java
+++ src/main/java/com/project/passwordmanager/controllers/restcontroller.java
@@ -3,5 +3,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class restcontroller {
-
+        @GetMapping("/deploy")
+        public String  rest() { return ""; }
 }
