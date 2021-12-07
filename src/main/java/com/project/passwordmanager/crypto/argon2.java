ddec21347b19d0b92515b0b86d75068a5b7e1e5b
HexaDrakon <bryantjinofficial@gmail.com>
2021-12-06 22:12:49 -0800

    build hack
    maven doesn't work on the deployment server? no problem! just use
    intellij to build it locally and then push it! this is definitely
    a good idea.

--- src/main/java/com/project/passwordmanager/crypto/argon2.java
+++ src/main/java/com/project/passwordmanager/crypto/argon2.java
@@ -2,7 +2,7 @@ package com.project.passwordmanager.crypto;
 import de.mkammerer.argon2.*;
 import java.security.NoSuchAlgorithmException;
 
-public class argon2 implements crypto {
+public class  argon2 implements crypto {
     public String hash(String toHash) throws NoSuchAlgorithmException {
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
         return argon2.hash(1, 37888, 1, toHash.toCharArray());
