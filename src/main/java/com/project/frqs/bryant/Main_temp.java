323968e74aed50ee755fe71acd90cc60aaaa6a53
HexaDrakon <bryantjinofficial@gmail.com>
2021-12-07 00:14:17 -0800

    i forgot that maven is picky
    temporarily worked my stuff into the primary controller because
    i am way too lazy to configure build profiles right now but i
    will definitely fix this later please remind me

--- src/main/java/com/project/frqs/bryant/Main_temp.java
+++ src/main/java/com/project/frqs/bryant/Main_temp.java
@@ -1,16 +1,12 @@
 package com.project.frqs.bryant;
 
-import com.project.passwordmanager.crypto.crypto;
-import com.project.passwordmanager.crypto.cryptoFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.security.NoSuchAlgorithmException;
-
 @SpringBootApplication
-public class Main {
+public class Main_temp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main_temp.class, args);
     }
 
 }
--- src/main/java/com/project/passwordmanager/controllers/controller.java
+++ src/main/java/com/project/passwordmanager/controllers/controller.java
@@ -16,6 +16,8 @@ import java.util.*;
 import org.json.*;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static com.project.frqs.bryant.LightSequence.calculate_sequence;
+
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class controller {
 
@@ -108,6 +110,14 @@ public class controller {
 
     @GetMapping("/about/rachel")
     public String rachel(){return "frontend/about-rachel"; }
+
+    // remove later
+    @GetMapping("/frq2")
+    public String nerd(@RequestParam(name = "a", required = false, defaultValue = "3") int a, @RequestParam(name = "b", required = false, defaultValue = "4") int b, @RequestParam(name = "initSeq", required = false, defaultValue = "0101 0101 0101") String initSeq, @RequestParam(name = "changeSeq", required = false, defaultValue = "0011 0011 0011") String changeSeq, @RequestParam(name = "segment", required = false, defaultValue = "1100000111") String segment, @RequestParam(name = "oldSeq", required = false, defaultValue = "00") String oldSeq, @RequestParam(name = "insertSeg", required = false, defaultValue = "1111 1111") String insertSeg, Model light_model) {
+        light_model.addAttribute("data", calculate_sequence(a, b, initSeq, changeSeq, segment, oldSeq, insertSeg));
+        light_model.addAttribute("desc", new String[]{"Initial display", "Changed display", "Display after insertion", "Replaced segment", "Distance between lights"});
+        return "frqs/frq2"; // returns HTML VIEW (greeting)
+    }
 
 
 }
