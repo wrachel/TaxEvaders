4e442d553b0cf6efdbfd4eaed827a0ed7f014442
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2021-12-13 09:06:40 -0800

    new kevin unit 2 frq 😩

--- src/main/java/com/project/passwordmanager/controllers/controller.java
+++ src/main/java/com/project/passwordmanager/controllers/controller.java
@@ -1,6 +1,6 @@
 package com.project.passwordmanager.controllers;
 
-import com.project.frqs.kevin.LightSequence;
+import com.project.passwordmanager.controllers.kevin.LightSequence;
 import org.json.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
@@ -12,15 +12,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.URI;
 import java.net.http.HttpResponse;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 
 import org.json.*;
-import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.project.frqs.bryant.LightSequence.calculate_sequence;
@@ -302,48 +299,6 @@ public class controller {
 
 
         return "frontend/about-rachel";
-    }
-
-    @GetMapping("/kevin/unit2")
-    public String displayFRQ(Model model) {
-
-        // declarations for later parts
-        String oldSeq = "1000 1100 1110";
-        String resultSeq;
-        String segment = "1110";
-
-        // PART (a)
-        LightSequence gradShow = new LightSequence("0101 0101 0101");
-        String parta = gradShow.getSeq();
-
-        // PART (b)
-        gradShow.display();
-
-        // PART (c)
-        gradShow.changeSequence("0011 0011 0011");
-        String newGradShow = gradShow.getSeq();
-
-        // PART (d)
-        resultSeq = gradShow.insertSegment("1111 1111", 4);
-
-        // PART (e)
-        int index = oldSeq.indexOf(segment);
-        String newSeq = oldSeq.substring(0, index) + oldSeq.substring(index + segment.length());
-
-        // PART (f)
-        int a = 3;
-        int b = 4;
-        double partf = Math.sqrt(a*a + b*b);
-
-        model.addAttribute("oldSeq", oldSeq);
-        model.addAttribute("segment", segment);
-        model.addAttribute("gradShow", parta);
-        model.addAttribute("newGradShow", newGradShow);
-        model.addAttribute("resultSeq", resultSeq);
-        model.addAttribute("newSeq", newSeq);
-        model.addAttribute("partf", partf);
-
-        return "frontend/kevin/unit2";
     }
 }
 
