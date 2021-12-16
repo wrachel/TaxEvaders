f82b4ae3d00dac469c7af80ba08c0ec1a110d76b
wrachel <Rwei951@gmail.com>
2021-12-15 17:23:36 -0800

    fixing my frq3

--- src/main/java/com/project/passwordmanager/controllers/rachel/rachelController.java
+++ src/main/java/com/project/passwordmanager/controllers/rachel/rachelController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
     public class rachelController extends rachelfrq3 {
+
     @GetMapping("/rachel/frq3")
     public String rachelfrq3(@RequestParam(name = "rsvp", required = false, defaultValue = "false") boolean rsvp, @RequestParam(name = "selection", required = false, defaultValue = "1") int selection, @RequestParam(name = "option1", required = false, defaultValue = "dog") String option1, @RequestParam(name = "option2", required = false, defaultValue = "dog") String option2, Model frq3model) {
         frq3model.addAttribute("rsvp", returnrsvp(rsvp));
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
         frq3model.addAttribute("message", rsvpfood(rsvp, selection));
         frq3model.addAttribute("stringMatch", checkString(option1, option2));
 
-        return "frontend/frqs/rachelfrq3";
+        return "frqs/rachelfrq3";
     }
 
 /*
@@ -65,7 +66,6 @@ import org.springframework.web.bind.annotation.RequestParam;
             return "not attending";
         }
     }
-
- */
+*/
 
 }
