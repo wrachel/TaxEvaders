162647cdfb576fd2ca4db2792d63cdd97472a691
wiz124 <hli16492@gmail.com>
2022-01-05 09:12:32 -0800

    me frq4 question 1

---
+++ src/main/java/com/project/passwordmanager/controllers/harry/frq4.java
@@ -0,0 +1,27 @@
+package com.project.passwordmanager.controllers.harry;
+
+public class frq4 {
+
+private char previousChar;
+private String largestStreak, currentStreak;
+
+    public String longestStreak(String str){
+        previousChar = ' ';
+
+         largestStreak = "";
+         currentStreak = "";
+
+         for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) != previousChar){
+                currentStreak = "";
+            }
+            currentStreak += str.charAt(i);
+            if (currentStreak.length() > largestStreak.length()){
+                largestStreak = currentStreak;
+            }
+            previousChar = str.charAt(i);
+        }
+        return largestStreak.charAt(0)+" "+" "+"length: "+largestStreak.length();
+    }
+
+}
