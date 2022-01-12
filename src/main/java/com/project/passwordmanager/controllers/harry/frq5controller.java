0091b62f5c066665c70f719fd48d183e2e32b539
wiz124 <hli16492@gmail.com>
2022-01-12 09:13:54 -0800

    frq5

--- src/main/java/com/project/passwordmanager/controllers/harry/frq5controller.java
+++ src/main/java/com/project/passwordmanager/controllers/harry/frq5controller.java
@@ -1,4 +1,15 @@
 package com.project.passwordmanager.controllers.harry;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 public class frq5controller {
+    @GetMapping("/harry/frq5a")
+    public String frq5q1(){
+        return "frqs/harryfrq5";
+    }
 }
