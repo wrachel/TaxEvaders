c2088ec75dfedcc60bbe7036223166d5501b301f
wiz124 <hli16492@gmail.com>
2022-01-13 09:06:38 -0800

    gimme ganyu

--- src/main/java/com/project/passwordmanager/controllers/harry/frq5controller.java
+++ src/main/java/com/project/passwordmanager/controllers/harry/frq5controller.java
@@ -6,10 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.io.IOException;
-public class frq5controller {
+
+@Controller
+public class frq5controller extends frq5 {
     @GetMapping("/harry/frq5a")
-    public String frq5q1(){
+    public String frq5q1(@RequestParam(name = "host", required = false, defaultValue = "karen") String host,
+                         @RequestParam(name = "add", required = false, defaultValue = "ad") String add,
+                         @RequestParam(name = "inv", required = false, defaultValue = "Cheryl") String inv,
+                         @RequestParam(name = "new", required = false, defaultValue = "address") String newadd ,Model model) throws IOException{
+
+        String c=invite(host,add,inv );
+
+        model.addAttribute("b", add);
+        model.addAttribute("a", host);
+        model.addAttribute("c",c);
+        model.addAttribute("d", newadd);
 
         return "frqs/harryfrq5";
     }
