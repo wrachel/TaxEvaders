5644643b0c361e46c1063512ffbb414da2c355b8
HexaDrakon <bryantjinofficial@gmail.com>
2022-01-20 04:47:24 -0800

    Implement FRQ 6 Part 2
    floating point errors uwu

---
+++ src/main/java/com/project/frqs/bryant/Payroll.java
@@ -0,0 +1,54 @@
+package com.project.frqs.bryant;
+
+import java.util.Arrays;
+
+public class Payroll {
+    private int[] itemsSold; // number of items sold by each employee
+    private double[] wages = new double[0]; // wages to be computed in part (b)
+
+    public double computeBonusThreshold() {
+        int[] minmax = findExtremes(itemsSold);
+        int min = minmax[0];
+        int max = minmax[1];
+        int sum = 0;
+        for (int items : itemsSold) {
+            sum += items;
+        }
+        sum -= (min + max);
+        return ((double) sum)/(itemsSold.length - 2);
+    }
+
+    public void computeWages(double fixedWage, double perItemWage) {
+        double wage;
+        double threshold = this.computeBonusThreshold();
+        for (int i = 0; i < itemsSold.length; i++) {
+            wage = (fixedWage + perItemWage * itemsSold[i]) * (itemsSold[i] > threshold ? 1.1 : 1.0);
+            if (i + 1 > this.wages.length) {
+                this.wages = Arrays.copyOf(this.wages, this.wages.length + 1);
+            }
+            this.wages[i] = wage;
+        }
+    }
+
+    private int[] findExtremes(int[] array) {
+        int min, max;
+        min = array[0];
+        max = array[0];
+        for (int value : array) {
+            min = Math.min(min, value);
+            max = Math.max(max, value);
+        }
+        return new int[] {min, max};
+    }
+
+    public Payroll(int[] itemsSold) {
+        this.itemsSold = itemsSold;
+    }
+
+    public static void main(String[] args) {
+        Payroll test = new Payroll(new int[] {48, 50, 37, 62, 38, 70, 55, 37, 64, 60});
+        System.out.println(test.computeBonusThreshold());
+        test.computeWages(10.0, 1.5);
+        System.out.println(Arrays.toString(test.wages));
+    }
+}
--- src/main/java/com/project/frqs/bryant/badGrammar.java
+++ src/main/java/com/project/frqs/bryant/badGrammar.java
@@ -1,5 +1,8 @@
 package com.project.frqs.bryant;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 public class badGrammar {
     private String[] words;
     public badGrammar(String[] words) {
@@ -7,12 +10,20 @@ public class badGrammar {
     }
 
     public String[] primitiveUnderstandingOfPresentParticiples() {
-        String[] presentParticiples = new String[this.words.length];
-        int index = 0;
+        String[] presentParticiples = new String[0];
         for (String word : this.words) {
-            presentParticiples[index] = word + "ing";
-            index += 1;
+            if (word.substring(word.length() - 3).equals("ing")) {
+                presentParticiples = Arrays.copyOf(presentParticiples, presentParticiples.length + 1);
+                presentParticiples[presentParticiples.length - 1] = word;
+            }
         }
         return presentParticiples;
+    }
+
+    public static void main(String[] args) {
+        badGrammar test = new badGrammar(new String[] {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"});
+        for (String participle : test.primitiveUnderstandingOfPresentParticiples()) {
+            System.out.println(participle);
+        }
     }
 }
