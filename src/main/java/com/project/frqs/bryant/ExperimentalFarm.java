60bb5a208751412a6451ef311acd703cefaef205
HexaDrakon <bryantjinofficial@gmail.com>
2022-01-26 09:44:26 -0800

    FRQ 8
    i think

---
+++ src/main/java/com/project/frqs/bryant/ExperimentalFarm.java
@@ -0,0 +1,58 @@
+import com.project.frqs.bryant.Plot;
+
+public class ExperimentalFarm {
+
+	private Plot[][] farmPlots;
+
+	public ExperimentalFarm(Plot[][] p) {
+		this.farmPlots = p;
+	}
+ 
+	/** Returns the plot with the highest yield for a given crop type, as described in part (a). */
+	public Plot getHighestYield(String c) {
+		int max = -1;
+		int max_x = -1;
+		int max_y = -1;
+		for (int i = 0; i < p.length; i++) {
+			for (int j = 0; j < p[0].length; j++) {
+				if (p[i][j].getCropType() == c) {
+					if (p[i][j].getCropYield() > max) {
+						max = p[i][j].getCropYield();
+						max_x = i;
+						max_y = j;
+					}
+				}
+			}
+		}
+		if (max_x + max_y == -2) {
+			return null
+		}
+		return p[max_x][max_y];
+	}
+
+	/** Returns true if all plots in a given column in the two-dimensional array farmPlots
+	* contain the same type of crop, or false otherwise, as described in part (b). */
+	public boolean sameCrop(int col) {
+		String cropType = p[0][col].getCropType();
+		
+		for (int i = 1; i < p.length; i++) {
+			if (!(p[i][col].equals(cropType))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Plot[][] plots = new {
+								{new Plot("corn", 20), new Plot("corn", 30), new Plot("peas", 10)},
+								{new Plot("peas", 30), new Plot("corn", 40), new Plot("corn", 62)},
+								{new Plot("wheat", 10), new Plot("corn", 50), new Plot("rice", 30)},
+								{new Plot("corn", 55), new Plot("corn", 30), new Plot("peas", 30)}
+							};
+		ExperimentalFarm farm = new ExperimentalFarm(plots);
+		System.out.println(farm.getHighestYield("corn"));
+		System.out.println(farm.sameCrop(0));
+		System.out.println(farm.sameCrop(1));
+    }
+}
---
+++ src/main/java/com/project/frqs/bryant/Plot.java
@@ -0,0 +1,18 @@
+public class Plot {
+
+	private String cropType;
+	private int cropYield;
+
+	public Plot(String crop, int yield) {
+		this.cropType = crop;
+		this.cropYield = yield;
+	}
+	
+	public String getCropType() {
+		return cropType;
+	}
+
+	public int getCropYield() {
+		return cropYield;
+	}
+}
