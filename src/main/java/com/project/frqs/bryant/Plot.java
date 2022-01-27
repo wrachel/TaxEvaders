7749919cf6db876d76c237c44c281cd6be4b2a36
HexaDrakon <bryantjinofficial@gmail.com>
2022-01-27 08:36:37 -0800

    Add tester methods and toString implementation
    just some nice quality of life changes

--- src/main/java/com/project/frqs/bryant/Plot.java
+++ src/main/java/com/project/frqs/bryant/Plot.java
@@ -16,4 +16,14 @@ public class Plot {
 	public int getCropYield() {
 		return cropYield;
 	}
+
+	public String toString() {
+		return String.format("This farm plants %s with a yield of %s", cropType, cropYield)
+	}
+
+	public static void main(String[] args) {
+		Plot plot = new Plot("corn", 10);
+		System.out.println(plot.getCropType());
+		System.out.println(plot.getCropYield());
+	}
 }
